e2c87b53dd06ebcdb032e9da6f130236eab84ef2
Nguyên Giáp <vngiap1998@gmail.com>
2023-06-26 23:51:29 +0700

    6

--- hmm/hmm/settings.py
+++ hmm/hmm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-my#=(*lkmngw_39ga$stissj!idfubr-$)ub55qyvh162h^k5x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
